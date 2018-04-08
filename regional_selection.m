0b99a28a72fc50eab8e75e533bb60b0a931bddf2
MES96 <MES96@users.noreply.github.com>
2018-04-08 14:56:17 +0100

    rs

--- regional_selection.m
+++ regional_selection.m
@@ -2,18 +2,14 @@
 
 load chanlocs2
 
-dname = 'Y:\Marie Shorrock\NTIP\Auditory Entrainment Study\Frequency';
+dname = 'Y:\Marie Shorrock\NTIP\Auditory Entrainment Study\Frequency\Region';
 entrainfiles = dir(fullfile(dname, '*entrain*'));
 
 cd(dname);
 
 av_area = [];
 
-%entrain = load('NTIP_0003_UNC_seqA_epoched_entrain_cleaned_Freq.mat'); %entrain.f.data
-%base = load('NTIP_0003_UNC_seqA_epoched_base_cleaned_Freq.mat'); % base.fdata
-
 for f= 1:length(entrainfiles)
-%for f = 1:length(entrain)
     
     entrainname = entrainfiles(f).name;
     basename = strrep(entrainname,'entrain','base');
@@ -41,17 +37,3 @@ for f= 1:length(entrainfiles)
     
     save NTIP_0001_average
 end
-%save NTIP_0003_UNC_seqA_entrainment_regions
-
-
-% file locations is 'dname', the files will be NTIP followed by the
-% participant number (0001, 0003, 0007, 0012, 0024-0032), followed by COM
-% or UNC, then seqA-seqC, then epoched_base or epoched_entrain, then cleaned_Freq. The base
-% one is so I can correct the entrainment data. The files end up looking
-% something like NTIP_0001_COM_seqA_epoched_base_cleaned_Freq.mat
-
-%At the moment it is saving the file for each participant (which is what I
-%would want) and then tries to do 0003 and gets stuck. So if you can figure
-%out how to get it to continue doing that, and how to get it to loop through and save the
-%file as something different for each participant that would be great. 
-%But I can't figure out how to get it to extract the file parts and then save it each time as a slightly different name.
